8fd19f0d43d0146c5458e412c7787a74497556b7
aith <ariiramanesh@gmail.com>
2021-02-16 09:12:17 -0800

    refactored and added multi-word strings for cycle

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+// TODO
+// Add a key mechanism (in the label or other)
+// Add more modular key-adding structure that uses a dict to store rule regexs with the functions
+// https://renenyffenegger.ch/notes/index.html
+
+// Graphviz tools https://renenyffenegger.ch/notes/tools/Graphviz/examples/index
 // SOCIAL NETWORK GENERATION
-const num_people = 10;
-const rel_types = ["friend", "lover", "enemy"];
 let seed;
 let dot;
 let dot_fragments = [];
 let text_lines = [];
 let arrow = '->'
 let regex_alphanum = /^[A-Za-z0-9]+$/
-// let cycleRule = /cycle\(,{3}\)/
-let cycleRule = /cycle\((.*?),(.*?),(.*?),(.*?)\)/
+
+let cycleRule = /cycle\((.*?),(.*?),(.*?),(.*?)\)/  // cycle(,,,,) with no white spaces for params
+let lockKeyRule = /lock\((.*?),(.*?)\)/
+let keyLocks = {}
 
 let social_edges = {};  // Looks like { srcname: {dst:[...], label: [...]}, ... }
 
@@ -56,48 +62,77 @@ function splitByNewline(str) {
 
 function generateDot(line) {
     let src, dst, label = "";
-    let is_valid_rule = false;
     if (checkIsArrow(line)) {
         // GET SRC & DST (->)
         let arr = (line.split(arrow, 2))
-        arr[0] = '"' + arr[0].replace(/[^0-9A-Za-z ]/, '').trim() + '"';  // remove non alphanumeric
-        arr[1] = '"' + arr[1].split(':', 1)[0].replace(/[^0-9A-Za-z ]/, '').trim() + '"';  // surroundig quotes for multi word
-
+        arr[0] = arr[0].replace(/[^0-9A-Za-z ]/, '').trim() ;  // remove non alphanumeric
+        arr[1] = arr[1].split(':', 1)[0].replace(/[^0-9A-Za-z ]/, '').trim();  // surroundig quotes for multi word
         // GET LABEL (:)
         if (/:/.test(line)) {
             label = " " + line.split(':', 2)[1].trim();
         }
         src = arr[0], dst = arr[1];
-        is_valid_rule = true;
+        print(src)
+        addEdge(social_edges, src, dst, label);
     }
     else if (cycleRule.test(line)) {
-        let n = line.split(',',4)
-        n[0] = n[0].substring(n[0].lastIndexOf('('), n[0].length).replace(/[^0-9A-Za-z ]/, '').trim();
-        n[1] = n[1].replace(/[^0-9A-Za-z ]/, '').trim();
-        n[2] = n[2].replace(/[^0-9A-Za-z ]/, '').trim();
-        n[3] = n[3].substring(0, n[3].indexOf(')')).replace(/[^0-9A-Za-z ]/, '').trim();
+        let c = 4;
+        let n = line.split(',',c)
+        n[0] = parseParamHead(n[0])
+        n[1] = parseParamBody(n[1])
+        n[2] = parseParamBody(n[2])
+        n[3] = parseParamTail(n[3]);
         if (!n.includes("")) {
-            for(let src = 0; src < 4;) {
-                addEdge(social_edges, n[src], n[++src % 4], "");
+            for(let src = 0; src < c;) {
+                addEdge(social_edges, n[src], n[++src % 4], '');
             }
         }
-        // add an edge from a to b to c to d
     }
-    if (is_valid_rule) addEdge(social_edges, src, dst, label);
-    // CONVERTION TO DOT LANGUAGE
+    else if (lockKeyRule.test(line)) {
+        let c = 2;
+        let n = line.split(',',c)
+        n[0] = parseParamHead(n)
+        n[1] = parseParamTail(n)
+        if (!n.includes("")) {
+            keyLocks[n[0]] = [n[1]]
+        }
+    }
+
 }
 
 function addEdge(dict, src, dst, label) {
-    if(!dict[src]) {
-        dict[src] = {  // check if dst has items already
+    if(!dict[src]) {  // CHECk IF DICT KEY-VAL PAIR IS OPEN
+        dict[src] = {  
             dst: [dst],
             label: [label]
         };
     }
-    else { // IF IT ALREADY EXISTS IN DICT, APPEND EDGE 
-        dict[src]["dst"].push(dst) ;
-        dict[src]["label"].push(label);
+    else { 
+        let idx = dict[src]["dst"].indexOf(dst)
+        if (idx == -1) { // PREVENT DUPLICATE EDGES, ALLOW REWRITES
+            dict[src]["dst"].push(dst) ;
+            dict[src]["label"].push(label);
+        }
+        else {
+            dict[src]["dst"][idx] = dst;
+            dict[src]["label"][idx] = label;
+        }
     }
+}
+
+function parseParamHead(n) {
+    n = n.substring(n.lastIndexOf('('), n.length).replace(/[^0-9A-Za-z ]/, '').trim();
+    return n;
+}
+
+function parseParamBody(n) {
+    n = n.replace(/[^0-9A-Za-z ]/, '').trim()
+    return n;
+}
+
+function parseParamTail(n) {
+    n = n.substring(0, n.indexOf(')')).replace(/[^0-9A-Za-z ]/, '').trim();
+    return n;
 }
 
 function render() {
@@ -105,7 +140,7 @@ function render() {
         for(let edge = 0; edge < social_edges[src]["dst"].length; edge++) {
             let dst = social_edges[src]["dst"][edge]
             let label = social_edges[src]["label"][edge]
-            dot_fragments.push(` ${src} -> ${dst} [label="${label}"]`);
+            dot_fragments.push(` "${src}" -> "${dst}" [label="${label}"]`);
         }
     }
 
