4b9e623e960485ad9077d3fe297969ee6589cff7
aith <ariiramanesh@gmail.com>
2021-02-15 22:39:32 -0800

    added starting example graph

--- script.js
+++ script.js
@@ -11,40 +11,40 @@ let regex_alphanum = /^[A-Za-z0-9]+$/
 let social_edges = [];
 
 function setup() {
-  numCols = select("#asciiBox").attribute("rows") | 0; // iot grab html element named asciiBox.
-  numRows = select("#asciiBox").attribute("cols") | 0; // 'select()' grabs an html element
-  select("#reseedButton").mousePressed(reseed);
-  select("#asciiBox").input(parseTextForm);
-  generateDot();
-  render();
+    numCols = select("#asciiBox").attribute("rows") | 0; // iot grab html element named asciiBox.
+    numRows = select("#asciiBox").attribute("cols") | 0; // 'select()' grabs an html element
+    select("#reseedButton").mousePressed(reseed);
+    select("#asciiBox").input(parseTextForm);
+    fillGrid();
+    parseTextForm()
 }
 
 function reseed() {
-  seed = (seed | 0) + 1109;
-  noiseSeed(seed);
-  randomSeed(seed);
-  select("#seedReport").html("seed " + seed);
+    seed = (seed | 0) + 1109;
+    noiseSeed(seed);
+    randomSeed(seed);
+    select("#seedReport").html("seed " + seed);
 }
 
-function regenerateGrid() {
-  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
-  parseTextForm();
+function fillGrid(text = "Hero -> Dragon : Fights\nDragon -> Treasure : Guards") {
+    select("#asciiBox").value(text);
+    text_lines.push(text)
 }
 
 function parseTextForm() {
     text_lines = splitByNewline(select("#asciiBox").value());
     // print(text_lines)
     // print(checkIsArrow(text_lines[0]))
-    generateDot();
+    text_lines.forEach(line => { generateDot(line) });
     render();
 }
 
 function gridToString(grid) {
-  let rows = [];
-  for (let i = 0; i < grid.length; i++) {
-    rows.push(grid[i].join(""));
-  }
-  return rows.join("\n");
+    let rows = [];
+    for (let i = 0; i < grid.length; i++) {
+        rows.push(grid[i].join(""));
+    }
+    return rows.join("\n");
 }
 
 function checkIsArrow(line) {
@@ -56,37 +56,36 @@ function checkIsArrow(line) {
 }
 
 function splitByNewline(str) {
-  let lines = str.split("\n");
-  return lines;
+    let lines = str.split("\n");
+    return lines;
 }
 
-function generateDot(lines) {
+function generateDot(line) {
     social_edges = []
-    text_lines.forEach(line => {
-        let src, dst, label = "";
-        let is_valid_rule = false;
-        if (checkIsArrow(line)) {
-            let arr = (line.split(arrow,2))
-            arr[0] = '"' + arr[0].replace( /[^0-9A-Za-z ]/, '').trim() + '"';  // remove non alphanumeric
-            arr[1] = '"' + arr[1].split(':',1)[0].replace( /[^0-9A-Za-z ]/, '').trim() + '"';  // surroundig quotes for multi word
-            if(/:/.test(line)) {
-                print("line is")
-                print(line)
-                label = line.split(':', 2)[1];
-            }
-            src = arr[0], dst = arr[1];
-            is_valid_rule = true;
+    let src, dst, label = "";
+    let is_valid_rule = false;
+    if (checkIsArrow(line)) {
+        // GET SRC & DST (->)
+        let arr = (line.split(arrow, 2))
+        arr[0] = '"' + arr[0].replace(/[^0-9A-Za-z ]/, '').trim() + '"';  // remove non alphanumeric
+        arr[1] = '"' + arr[1].split(':', 1)[0].replace(/[^0-9A-Za-z ]/, '').trim() + '"';  // surroundig quotes for multi word
+
+        // GET LABEL (:)
+        if (/:/.test(line)) {
+            label = " " + line.split(':', 2)[1].trim();
         }
-        if(is_valid_rule) social_edges.push({
-            src: src,
-            dst: dst,
-            label: label
-        });
+        src = arr[0], dst = arr[1];
+        is_valid_rule = true;
+    }
+    if (is_valid_rule) social_edges.push({
+        src: src,
+        dst: dst,
+        label: label
     });
 
     // CONVERTION TO DOT LANGUAGE
     for (let obj of social_edges) {
-        let {src, dst, label} = obj;
+        let { src, dst, label } = obj;
         dot_fragments.push(` ${src} -> ${dst} [label="${label}"]`);
     }
 }
@@ -102,10 +101,10 @@ function render() {
     dot_fragments = []
 
 
-// hpccWasm.graphvizSync().then(graphviz => {
-//     const div = document.getElementById("placeholder2");
-//     // Synchronous call to layout
-//     div.innerHTML = graphviz.layout(dot, "svg", "dot");
-// });
+    // hpccWasm.graphvizSync().then(graphviz => {
+    //     const div = document.getElementById("placeholder2");
+    //     // Synchronous call to layout
+    //     div.innerHTML = graphviz.layout(dot, "svg", "dot");
+    // });
 }
 
